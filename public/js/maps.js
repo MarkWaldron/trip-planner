128451a2593505f17b29adca9ceae2979bc70cbf
MarkWaldron <markewaldron@gmail.com>
2015-11-24 16:42:54 -0500

    Added more styling

--- public/js/maps.js
+++ public/js/maps.js
@@ -3,11 +3,16 @@
 function initialize_gmaps() {
     // initialize new google maps LatLng object
     var myLatlng = new google.maps.LatLng(40.705189,-74.009209);
+
+    var styles = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]}]
+
+    var styledMap = new google.maps.StyledMapType(styles,
+        {name: "Styled Map"})
     // set the map options hash
     var mapOptions = {
         center: myLatlng,
         zoom: 16,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
     };
     // get the maps div's HTML obj
     var map_canvas_obj = document.getElementById("map-canvas");
@@ -20,6 +25,8 @@ function initialize_gmaps() {
     });
     // Add the marker to the map by calling setMap()
     marker.setMap(map);
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
 }
 
 $(document).ready(function() {
